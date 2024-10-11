a803a684bdbd233305fd9f18bc2699b3bfa2f71b
Ozie Virto <ovirto@outlook.com>
2024-10-11 00:35:47 -0500

    SwitchAccount converted to POST

--- MyFirstAzureFunction/MyFirstAzureFunction/Functions/SwitchAccount.cs
+++ MyFirstAzureFunction/MyFirstAzureFunction/Functions/SwitchAccount.cs
@@ -1,7 +1,6 @@
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker.Http;
 using MyFirstAzureFunction.Implementations.Services;
@@ -22,15 +21,23 @@ public class SwitchAccount
     }
     
     [Function("SwitchAccount")]
-    public async Task<IActionResult> GetAccounts(
-        [HttpTrigger(AuthorizationLevel.Function, "put", Route = "accounts/switch/{userId}/{accountId}")] HttpRequestData req,
-        string userId,
-        int accountId,
+    public async Task<IActionResult> GetAccount(
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = "accounts/switch")] HttpRequestData req,
         ILogger log)
     {
-        await _accountService.SwitchAccountAsync(userId, accountId);
+        // Read and deserialize the request body
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var requestData = JsonConvert.DeserializeObject<SwitchAccountRequest>(requestBody);
+            
+        if (requestData == null || string.IsNullOrEmpty(requestData.UserId) || requestData.AccountId <= 0)
+        {
+            return new BadRequestObjectResult("Invalid request data.");
+        }
 
-        string message = $"Account switched to account with ID of {accountId} successfully!";
-        return new OkObjectResult(new { Message = message});
+        // Call the service to switch the account
+        await _accountService.SwitchAccountAsync(requestData.UserId, requestData.AccountId);
+
+        string message = $"Account switched to account with ID of {requestData.AccountId} successfully!";
+        return new OkObjectResult(new { Message = message });
     }
 }
