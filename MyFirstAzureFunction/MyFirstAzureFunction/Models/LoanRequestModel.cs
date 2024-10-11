9781e6d2adb6160c9cab9b44b75180d361d56c89
Quinn2328 <113200011+Ivorysdead@users.noreply.github.com>
2024-10-11 08:17:28 -0500

    Revert "Revert "Fix Merge Main Deletion ""

--- MyFirstAzureFunction/MyFirstAzureFunction/Models/LoanRequestModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFirstAzureFunction.Models;
-
-public class LoanRequestModel
-{
-    public int LoanId { get; set; }
-    public int UserId { get; set; }
-    public string LoanName { get; set; }
-    public double LoanAmount { get; set; }
-}
